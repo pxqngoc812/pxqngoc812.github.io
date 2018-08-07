d9512c030a104735753112a7405fe5622490464f
pxqngoc812 <pxqngoc812@gmail.com>
2018-08-07 17:07:49 +0700

    1.5

--- memorycard1/script.js
+++ memorycard1/script.js
@@ -1,19 +1,21 @@
 var photoarr = ["p0", "p1", "p2", "p3", "p4", "p5", "p6"];
 var photo = "";
+var currentCard = null;
 
 photoarrx2 = photoarr.concat(photoarr);
 shuffle(photoarrx2);
 
 function addPhoto() {
     for(var i=0; i<photoarrx2.length; i++) {
-        photo += '<div class="card" onclick="flipp(this)"><div class="back"><img src="img/back.gif"></div><div class="front"><img src="img/'+photoarrx2[i]+'.jpg"></div></div>';
+        photo += '<div class="card" onClick="flipp(this)" data-name="'+photoarrx2[i]+'"><div class="back"><img src="img/back.gif"></div><div class="front"><img src="img/'+photoarrx2[i]+'.jpg"></div></div>';
     }
-    
     $(".content").html(photo);
 }
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
 
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
@@ -26,7 +28,7 @@ function shuffle(array) {
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
-  }
+    }
 
   return array;
 }
@@ -34,5 +36,41 @@ function shuffle(array) {
 addPhoto();
 
 function flipp(card) {
-    $(card).addClass("flip");
+    $(card).toggleClass("flip");
+    if (!currentCard) {
+        currentCard = $(card);
+        currentCard.css("pointer-events","none")
+        console.log("1");
+    }
+    else {
+        if(currentCard.attr('data-name') != $(card).attr('data-name')) {
+            // 2 card khac nhau
+            
+            console.log(currentCard.attr('data-name'));
+            console.log($(card).attr('data-name'));
+
+            setTimeout(function(){
+                currentCard.toggleClass("flip");
+                $(card).toggleClass("flip");
+                currentCard = null;
+                currentCard.css("pointer-events","auto")
+            }, 500);  
+            currentCard.css("pointer-events","auto")
+
+        }
+        
+        else {
+            // 2 card giong nhau
+            console.log(currentCard.attr('data-name'));
+            console.log($(card).attr('data-name'));
+            
+            setTimeout(function(){
+                $(card).css("opacity", "0");
+                currentCard.css("opacity", "0");
+                currentCard = null;
+            }, 250);
+
+            console.log("giong");
+        }
+    }
 }    
